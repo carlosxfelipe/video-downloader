08ea78e23409061b9f57fd8dae115d2a99677b53
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-09-11 23:56:37 -0300

    First commit

---
+++ VideoDownloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoDownloader
+//
+//  Created by Carlos Felipe Araújo on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoDownloader/VideoDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDownloaderApp.swift
+//  VideoDownloader
+//
+//  Created by Carlos Felipe Araújo on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
